1721d0d44f0c3c30da61cd7daf0cc96a8daa9630
allenarduino <aljay3334@gmail.com>
2024-08-17 13:42:31 +0000

    feature: Create user-auth backend api (#3)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'my_db_name',
+      password: 'my_db_password',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ backend/src/auth/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+// src/auth/dto/create-user.dto.ts
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  readonly password: string;
+}
