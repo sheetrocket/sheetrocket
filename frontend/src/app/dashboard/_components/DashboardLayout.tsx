e2810063816faba3d37de3902b094840f08c5ebb
allenarduino <aljay3334@gmail.com>
2024-10-02 21:14:05 +0000

    feature: Add custom style to the dasboard layout(sidenav and appBar) (#17)

--- frontend/src/app/dashboard/_components/DashboardLayout.tsx
+++ frontend/src/app/dashboard/_components/DashboardLayout.tsx
@@ -16,6 +16,11 @@ import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import * as Icon from "react-feather"; // Importing react-feather icons
 import { useEffect, useState } from "react";
+import Tooltip from "@mui/material/Tooltip";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { usePathname } from "next/navigation";
 
 const drawerWidth = 240;
 
@@ -26,22 +31,29 @@ interface Props {
 // Sidebar data with react-feather icons
 const sidebarData = [
   {
-    name: "Spreadsheets",
+    name: "Spreadsheets APIs",
     icon: <Icon.File />,
     routeName: "/dashboard/spreadsheets",
   },
   { name: "Usage", icon: <Icon.BarChart />, routeName: "/dashboard/usage" },
-  { name: "Plan", icon: <Icon.CreditCard />, routeName: "/dashboard/plan" },
-  {
-    name: "Add Spreadsheet",
-    icon: <Icon.PlusSquare />,
-    routeName: "/dashboard/add-spreadsheet",
-  },
 ];
 
 export default function DashboardLayout({ children }: Props) {
+  const currentPath = usePathname();
   const [mobileOpen, setMobileOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false); // Track client-side mount
+
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -53,16 +65,35 @@ export default function DashboardLayout({ children }: Props) {
 
   const drawer = (
     <>
-      <Toolbar />
+      <Toolbar sx={{ marginTop: "20px" }} />
       <List>
         {sidebarData.map((item) => (
-          <ListItem key={item.name} disablePadding>
+          <ListItem key={item.name}>
             <Link href={item.routeName} className='w-[100%]'>
-              <ListItemButton component='span'>
-                <ListItemIcon sx={{ minWidth: "40px" }}>
+              <ListItemButton
+                component='span'
+                sx={{
+                  backgroundColor:
+                    currentPath === item.routeName ? "teal" : "transparent",
+                  color: currentPath === item.routeName ? "#fff" : "inherit",
+                  borderRadius: currentPath === item.routeName ? "8px" : "",
+                  "&:hover": {
+                    backgroundColor: "teal",
+                    borderRadius: "8px",
+                    color: "#fff",
+                  },
+                }}
+              >
+                <ListItemIcon sx={{ minWidth: "40px", color: "inherit" }}>
                   {item.icon}
                 </ListItemIcon>
-                <ListItemText primary={item.name} />
+                <ListItemText
+                  primary={item.name}
+                  primaryTypographyProps={{
+                    fontSize: "13px",
+                    fontWeight: "400",
+                  }}
+                />
               </ListItemButton>
             </Link>
           </ListItem>
@@ -81,7 +112,10 @@ export default function DashboardLayout({ children }: Props) {
       <AppBar
         position='fixed'
         sx={{
+          backgroundColor: "#fff",
           zIndex: (theme) => theme.zIndex.drawer + 1,
+          boxShadow: "none",
+          borderBottom: "1px solid #3333",
         }}
       >
         <Toolbar>
@@ -90,13 +124,55 @@ export default function DashboardLayout({ children }: Props) {
             aria-label='open drawer'
             edge='start'
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { md: "none" } }}
+            sx={{ mr: 2, display: { md: "none" }, color: "teal" }}
           >
             <MenuOutlined />
           </IconButton>
-          <Typography variant='h6' noWrap component='div'>
-            Dashboard
+          <Typography
+            sx={{
+              textAlign: "center",
+              color: "teal",
+              fontWeight: "bold",
+              fontSize: "18px",
+              fontStyle: "oblique",
+            }}
+          >
+            <Link href='/'>Sheetrocket</Link>
           </Typography>
+          <Box
+            sx={{
+              flex: 1,
+              display: "flex",
+              alignItems: "flex-end",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Tooltip title='Open settings'>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt='User avatar' src='' />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id='menu-appbar'
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Typography sx={{ textAlign: "center" }}>Logout</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
       <Box
